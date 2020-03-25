983e34ec500913b89db71c870aca1aea2e74d7cf
OtvalPizdy <ya.tolchennikov2013@yandex.ru>
2020-03-26 02:14:26 +0500

    13

---
+++ PigGameProject/PigRun/Assets/AppleSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleSpawner : MonoBehaviour
+{
+    public Transform SpawnPos;
+    public GameObject apple;
+    public float TimeSpawn;
+    void Start()
+    {
+        StartCoroutine(SpawnCD());
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(SpawnCD());
+    }
+    IEnumerator SpawnCD()
+    {
+        yield return new WaitForSeconds(TimeSpawn);
+        Instantiate(apple, SpawnPos.position, Quaternion.identity);
+        Repeat();
+    }
+
+}
