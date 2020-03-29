22462a05a846294f29c44f0e78ecfde090b01903
OtvalPizdy <ya.tolchennikov2013@yandex.ru>
2020-03-29 16:10:08 +0500

    PauseMenuComplete(BugDestroyed)

--- PigGameProject/PigRun/Assets/PauseMenu.cs
+++ PigGameProject/PigRun/Assets/PauseMenu.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-
+    public GameObject continue_button;
     public GameObject pauseMenuUI;
+    private RectTransform rectTransfrom;
+
 
     // Update is called once per frame
     void Update()
@@ -25,9 +28,15 @@ public class PauseMenu : MonoBehaviour
     }
     public void Resume()
     {
+        rectTransfrom = continue_button.GetComponent<RectTransform>();
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        //button1.texture.Resize(640, 480);
+        rectTransfrom.sizeDelta = new Vector2(345.7f, 64.8f); // задаем размер   new Vectro2 (width, height)
+        //continue_button.transform.
+           //gameObject.transform.
+        //localScale = new Vector2(1f, 1f);
     }
 
     void Pause()
