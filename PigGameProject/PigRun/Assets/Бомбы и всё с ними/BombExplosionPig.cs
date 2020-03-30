426ea01a4451fc6bee0c5468f0f56912f5930772
OtvalPizdy <ya.tolchennikov2013@yandex.ru>
2020-03-30 17:17:34 +0500

    Animation need fix

--- PigGameProject/PigRun/Assets/Бомбы и всё с ними/BombExplosionPig.cs
+++ PigGameProject/PigRun/Assets/Бомбы и всё с ними/BombExplosionPig.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombExplosionPig : MonoBehaviour
-
-    private bool IsExplosive;
+public class BombExplosionPig : MonoBehaviour  
 
 {
+    public bool IsExplosive;
+    private Animator anim;
 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Pig")
         {
-            IsExplosive = true;
-            Destroy(gameObject);
+            anim.SetBool("IsExplosive", true);
+            //Destroy(gameObject);
         }
         else if ((collision.gameObject.name == "TransparentVertical") || (collision.gameObject.name == "TransparentVertical1") || (collision.gameObject.name == "TransparentHorizontal") || (collision.gameObject.name == "TransparentHorizontal1"))
         {
