4c4e62189431c5a64c688727996505ef4593cacc
OtvalPizdy <ya.tolchennikov2013@yandex.ru>
2020-03-30 17:00:18 +0500

    20 need fix

---
+++ PigGameProject/PigRun/Assets/Бомбы и всё с ними/BombExplosionPig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosionPig : MonoBehaviour
+
+    private bool IsExplosive;
+
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Pig")
+        {
+            IsExplosive = true;
+            Destroy(gameObject);
+        }
+        else if ((collision.gameObject.name == "TransparentVertical") || (collision.gameObject.name == "TransparentVertical1") || (collision.gameObject.name == "TransparentHorizontal") || (collision.gameObject.name == "TransparentHorizontal1"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PigGameProject/PigRun/Assets/Бомбы и всё с ними/BompSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BompSpawn : MonoBehaviour
+{
+    public GameObject Bomb;
+    public float TimeSpawn;
+    void Start()
+    {
+        StartCoroutine(SpawnBomb());
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(SpawnBomb());
+    }
+    IEnumerator SpawnBomb()
+    {
+        yield return new WaitForSeconds(TimeSpawn);
+        Instantiate(Bomb, new Vector2(Random.Range(-8.3f, 8.2f), 6f), Quaternion.identity);
+        Repeat();
+    }
+
+}
